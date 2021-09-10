607ba0edfe1aae9b5b8233fa024b19f16c831bc7
cori <cori@glitch.com>
2021-09-09 22:51:26 -0500

    revert to Oryx config with no layer 1 color

--- keyboards/moonlander/keymaps/cori/config.h
+++ keyboards/moonlander/keymaps/cori/config.h
@@ -2,18 +2,12 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-// #define ORYX_CONFIGURATOR
+#define ORYX_CONFIGURATOR
 #undef TAPPING_TERM
 #define TAPPING_TERM 225
 
 #define RETRO_TAPPING
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-// #define FIRMWARE_VERSION u8"z9xWM/j7VYx"
+#define FIRMWARE_VERSION u8"z9xWM/GRJ63"
 #define RGB_MATRIX_STARTUP_SPD 60
-
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define RGB_MATRIX_KEYPRESSES  // reacts to keypresses
-#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_SIMPLE  //  RGB_MATRIX_SOLID_REACTIVE_NEXUS  // Sets the default mode, if none has been set
-#define RGBLIGHT_ANIMATIONS             // Run RGB animations
